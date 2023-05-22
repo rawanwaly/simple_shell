66e70781a14f6be894eac67e5521c2891626355e
omaradelp4er <omaradelp4er@gmail.com>
2023-05-22 09:15:01 -0700

    Updated

--- history.c
+++ history.c
@@ -1,5 +1,11 @@
 #include "shell.h"
 
+/**
+ * get_history_file - gets the history file
+ * @info: parameter struct
+ *
+ * Return: allocated string containg history file
+ */
 
 char *get_history_file(info_t *info)
 {
@@ -18,7 +24,12 @@ char *get_history_file(info_t *info)
 	return (buf);
 }
 
-
+/**
+ * write_history - creates a file, or appends to an existing file
+ * @info: the parameter struct
+ *
+ * Return: 1 on success, else -1
+ */
 int write_history(info_t *info)
 {
 	ssize_t fd;
@@ -42,6 +53,12 @@ int write_history(info_t *info)
 	return (1);
 }
 
+/**
+ * read_history - reads history from file
+ * @info: the parameter struct
+ *
+ * Return: histcount on success, 0 otherwise
+ */
 int read_history(info_t *info)
 {
 	int i, last = 0, linecount = 0;
@@ -85,7 +102,14 @@ int read_history(info_t *info)
 	return (info->histcount);
 }
 
-
+/**
+ * build_history_list - adds entry to a history linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ * @buf: buffer
+ * @linecount: the history linecount, histcount
+ *
+ * Return: Always 0
+ */
 int build_history_list(info_t *info, char *buf, int linecount)
 {
 	list_t *node = NULL;
@@ -99,7 +123,12 @@ int build_history_list(info_t *info, char *buf, int linecount)
 	return (0);
 }
 
-
+/**
+ * renumber_history - renumbers the history linked list after changes
+ * @info: Structure containing potential arguments. Used to maintain
+ *
+ * Return: the new histcount
+ */
 int renumber_history(info_t *info)
 {
 	list_t *node = info->history;
